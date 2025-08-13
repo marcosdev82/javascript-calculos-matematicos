9abd088f164cdae276d5c83f58f83c360a212aa6
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-13 11:52:40 -0300

    métodos estáticos

---
+++ metodos estaticos/abs.js
@@ -0,0 +1,14 @@
+// Métodos Estáticos do Objeto Math no JavaScript
+
+// A função Math.abs(x) retorna o valor absoluto de um número "x", tal qual:
+
+Math.abs("-1"); // 1
+Math.abs(-2); // 2
+Math.abs(null); // 0
+Math.abs(""); // 0
+Math.abs([]); // 0
+Math.abs([2]); // 2
+Math.abs([1, 2]); // NaN
+Math.abs({}); // NaN
+Math.abs("string"); // NaN
+Math.abs(); // NaN
